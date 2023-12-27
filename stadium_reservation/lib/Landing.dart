344b01a3756525994fd9bca108e04a105a241701
Deshal-001 <85722430+Deshal-001@users.noreply.github.com>
2023-12-27 16:10:05 +0530

    Landing Page

--- stadium_reservation/lib/Landing.dart
+++ stadium_reservation/lib/Landing.dart
@@ -77,7 +77,7 @@ class LandingPage extends StatelessWidget {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
                       ),
                       child: const Text(
                         'One-Time Booking',
@@ -129,7 +129,7 @@ class LandingPage extends StatelessWidget {
                   enlargeCenterPage: true,
                   autoPlay: true,
                 ),
-                items: [
+                items: const [
                   FeaturedStadiumWidget(
                       imageUrl:
                       'https://images.unsplash.com/photo-1595030044556-acfaa61edc0f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
